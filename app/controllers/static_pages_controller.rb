1dd71fcae71a49e66ad77720e2cb11b5cba97b9c
steven <steven.h@live.com>
2021-02-12 21:39:42 -0800

    update routes
    add flash (for errors) to view
    add form asking for user id to view
    add loop to display user's photos to view
    
    add strong param and finish index action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
+    @flickr = Flickr.new
+    @user_id = id_param[:user_id]
+
+    begin
+      @photos = @flickr.people.getPhotos(user_id: @user_id.strip)
+    rescue
+      flash.now[:error] = "#{@user_id} not found!"
+      render :index
+    end
+  end
+
+  private
+
+  def id_param
+    params.permit(:user_id, :commit)
   end
 end
